0de11debff173296dd287fa37dfede8a1c4b94cb
hrs1408 <hieu.nguyenphanhuy@students.hueic.edu.vn>
2023-02-22 18:47:46 +0700

    update: 10-14

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ x = math.pi / 4
 print(sin(x))  # Kết quả phải gần bằng math.sin(x) = math.sin(pi/4) = 0.7071067811865476
 
 
-#7
+# 7
 def cos(x, terms=10):
     """Tính giá trị của hàm cos(x) bằng chuỗi Taylor với số lượng terms"""
     result = 0
@@ -148,32 +148,28 @@ x = math.pi / 4
 print(cos(x))  # Kết quả phải gần bằng math.cos(x) = math.cos(pi/4) = 0.7071067811865476
 
 
-#8
-import math
-
+# 8
 def arcsin(x, terms=10):
     """Tính giá trị của hàm arcsin(x) bằng chuỗi Taylor với số lượng terms"""
     result = 0
     for n in range(terms):
         numerator = 1
-        for i in range(2*n):
+        for i in range(2 * n):
             numerator *= i + 1
             if i % 2 == 1:
                 numerator *= x
-        denominator = 2**(2*n) * math.factorial(n)**2 * (2*n + 1)
+        denominator = 2 ** (2 * n) * math.factorial(n) ** 2 * (2 * n + 1)
         term = numerator / denominator
         result += term
     return result
 
+
 # Kiểm tra với giá trị x = 0.5
 x = 0.5
-print(arcsin(x)) # Kết quả phải gần bằng math.asin(x) = math.asin(0.5) = 0.5235987755982989
+print(arcsin(x))  # Kết quả phải gần bằng math.asin(x) = math.asin(0.5) = 0.5235987755982989
 
 
-#9
-import math
-
-
+# 9
 def sin_over_x(x, n):
     """
     Tính giá trị sin(x)/x với n số lượng các số hạng của chuỗi Taylor
@@ -197,6 +193,124 @@ x = 1
 n = 5
 print(f"sin({x})/{x} = {sin_over_x(x, n)}")
 print(f"Giá trị chính xác: {math.sin(x) / x}")
+
+
+# 10
+def arctan(x, n):
+    result = 0
+    for i in range(n):
+        sign = (-1) ** i
+        term = x ** (2 * i + 1) / (2 * i + 1)
+        result += sign * term
+    return result
+
+
+x = 0.5  # Giá trị của x
+n = 10  # Số lượng các số hạng cần tính
+
+# Tính giá trị của arctan(x) theo công thức
+result = arctan(x, n)
+
+# Tính giá trị chính xác của arctan(x) bằng hàm của thư viện math
+exact_result = math.atan(x)
+
+# In ra kết quả
+print(f"arctan({x}) = {result}")
+print(f"Giá trị chính xác của arctan({x}) = {exact_result}")
+
+
+# 11
+def ln_1plusx(x, n):
+    result = 0
+    sign = 1
+    for i in range(1, n + 1):
+        term = (x ** i) / i
+        result += sign * term
+        sign *= -1
+    return result
+
+
+x = 0.5  # Giá trị của x
+n = 10  # Số lượng các số hạng cần tính
+
+# Tính giá trị của ln(1+x) theo công thức
+result = math.log(1 + x)
+
+# Tính giá trị của ln(1+x) bằng cách tính tổng các số hạng
+approx_result = ln_1plusx(x, n)
+
+# In ra kết quả
+print(f"ln(1+{x}) = {result}")
+print(f"Giá trị xấp xỉ của ln(1+{x}) = {approx_result}")
+
+
+# 12
+def ln(x, n):
+    result = 0
+    for i in range(n):
+        term = (x ** (2 * i + 1)) / (2 * i + 1)
+        result += term
+    return 2 * result
+
+
+x = 0.5  # Giá trị của x
+n = 10  # Số lượng các số hạng cần tính
+
+# Tính giá trị của ln(1+x/1-x) theo công thức
+result = ln(x, n)
+
+# Tính giá trị chính xác của ln(1+x/1-x) bằng hàm của thư viện math
+exact_result = math.log((1 + x) / (1 - x))
+
+# In ra kết quả
+print(f"ln(1+{x}/1-{x}) = {result}")
+print(f"Giá trị chính xác của ln(1+{x}/1-{x}) = {exact_result}")
+
+
+# 13
+def sinh(x, n):
+    result = 0
+    for i in range(n):
+        term = (x ** (2 * i + 1)) / math.factorial(2 * i + 1)
+        result += term
+    return result
+
+
+x = 1.5  # Giá trị của x
+n = 10  # Số lượng các số hạng cần tính
+
+# Tính giá trị của sinh x theo công thức
+result = (math.exp(x) - math.exp(-x)) / 2
+
+# Tính giá trị của sinh x bằng cách tính tổng các số hạng
+approx_result = sinh(x, n)
+
+# In ra kết quả
+print(f"sinh({x}) = {result}")
+print(f"Giá trị xấp xỉ của sinh({x}) = {approx_result}")
+
+
+# 14
+def cosh(x, n):
+    result = 0
+    for i in range(n):
+        term = (x ** (2 * i)) / math.factorial(2 * i)
+        result += term
+    return result
+
+
+x = 1.5  # Giá trị của x
+n = 10  # Số lượng các số hạng cần tính
+
+# Tính giá trị của cosh x theo công thức
+result = (math.exp(x) + math.exp(-x)) / 2
+
+# Tính giá trị của cosh x bằng cách tính tổng các số hạng
+approx_result = cosh(x, n)
+
+# In ra kết quả
+print(f"cosh({x}) = {result}")
+print(f"Giá trị xấp xỉ của cosh({x}) = {approx_result}")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
