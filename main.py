84b07ee852c73482f70c0e52c13fbb785d1d0568
hrs1408 <hieu.nguyenphanhuy@students.hueic.edu.vn>
2023-02-24 17:21:24 +0700

    update exercise

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ import math
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(title):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+    print(f'{title}')  # Press F9 to toggle the breakpoint.
+
+
+eps = 0.00001
 
 
 # 1
-def exp_taylor(x):
-    # Khởi tạo giá trị ban đầu của biến k, giá trị của chuỗi và của mỗi số hạng trong chuỗi
+def exercise_one(x):
     k = 0
     s = 1
     term = 1
 
-    # Lặp cho đến khi đạt được độ chính xác mong muốn
     while abs(term) > 1e-8:
-        # Tính giá trị của số hạng tiếp theo trong chuỗi Taylor
         k += 1
         term *= x / k
         s += term
@@ -28,292 +28,296 @@ def exp_taylor(x):
     return s
 
 
-# Nhập giá trị của x từ bàn phím và tính giá trị của e^x bằng hàm exp_taylor
-x = float(input("Nhập giá trị của x: "))
-result = exp_taylor(x)
-
-# So sánh kết quả với giá trị tính được bởi hàm math.exp của Python
-expected_result = math.exp(x)
-print(f"Giá trị của e^{x} là {result:.8f}")
-print(f"Kết quả mong đợi là {expected_result:.8f}")
+# Kiểm tra với giá trị x = 5
+print(f"Example One: {exercise_one(float(5)):.8f}")
 
 
 # 2
-def maclaurin(x, n):
-    # Khởi tạo biến đếm và giá trị của chuỗi
-    k = 1
-    s = 1
+def exercise_two(x):
+    i = 1
+    first = 1
+    second = first - ((((i + 1) * (i + 2)) / 2) * x ** i)
 
-    # Tính giá trị của từng số hạng trong chuỗi Maclaurin
-    while k < n:
-        # Tính các hệ số
-        a = k + 1
-        b = k + 2
-        c = 2
+    while abs(first - second) > eps:
+        i += 1
+        first = second
 
-        # Tính số hạng trong chuỗi
-        term = (-1) ** k * a * b / c * x ** (3 * k)
-        s += term
-        k += 1
+        if i % 2 == 0:
+            second = first + ((((i + 1) * (i + 2)) / 2) * x ** i)
+        else:
+            second = first - ((((i + 1) * (i + 2)) / 2) * x ** i)
 
-    return s
+    return first
 
 
-# Nhập giá trị của x từ bàn phím
-x = float(input("Nhập giá trị của x: "))
+# Kiểm tra với giá trị x = 0.5
+print(f"Example Two: {exercise_two(0.5)}")
 
-# Tính giá trị của 1/(1+x^3) với 10 số hạng đầu tiên của chuỗi Maclaurin
-n = 10
-result = maclaurin(x, n)
 
-# In ra kết quả
-print(f"Giá trị của 1/(1+x^3) là {result}")
+# 3
+def exercise_three(x):
+    i = 1
+    first = -x
+    second = first - ((x ** (i + 1)) / (i + 1))
+
+    while abs(second - first) > 0.0001:
+        i += 1
+        first = second
+        second = first - ((x ** (i + 1)) / (i + 1))
+
+    return first
+
+
+print(f"Example Three: {exercise_three(float(-1))}")
 
 
 # 4
-def sqrt_1_plus_x(x, terms=10):
-    """Tính căn bậc hai của (1 + x) bằng chuỗi Taylor với số lượng terms"""
-    result = 0
-    for n in range(terms):
-        numerator = 1
-        denominator = 1
-        for i in range(n):
-            numerator *= (2 * i + 1)
-            denominator *= (2 * i + 2)
-        term = ((-1) ** n * numerator * x ** (n + 1)) / (denominator * 2 ** n)
-        result += term
-    return result + 1
+def exercise_four(x):
+    i = 1
+    first = 1
+    numerator = 1
+    denominator = 2
+
+    second = first + (numerator / denominator) * x
+    numerator_step = 1
+    denominator_step = 4
+
+    while abs(first - second) > eps:
+        i += 1
+        first = second
+
+        numerator *= numerator_step
+        denominator *= denominator_step
+
+        numerator_step += 2
+        denominator_step += 2
+
+        if i % 2 == 0:
+            second -= (numerator / denominator) * x ** i
+        else:
+            second += (numerator / denominator) * x ** i
+
+    return first
 
 
-# Kiểm tra với giá trị x = 0.5
-x = 0.5
-print(sqrt_1_plus_x(x))  # Kết quả phải gần bằng math.sqrt(1+x) = math.sqrt(1.5) = 1.224744871391589
+print(f"Example Four: {exercise_four(float(0.5))}")
 
 
 # 5
-def inv_sqrt_1_plus_x(x, terms=10):
-    """Tính nghịch đảo căn bậc hai của (1 + x) bằng chuỗi Taylor với số lượng terms"""
-    result = 1
-    for n in range(1, terms):
-        numerator = 1
-        denominator = 1
-        for i in range(n):
-            numerator *= (2 * i - 1) if i > 0 else (-1)
-            denominator *= (2 * i) if i > 0 else 1
-        term = ((-1) ** n * numerator * x ** n) / (denominator * 2 ** (n - 1))
-        result += term
-    return result
+def exercise_five(x):
+    i = 1
+    first = 1
+    numerator = 1
+    denominator = 2
+    numerator_step = 3
+    denominator_step = 4
+
+    second = first - (numerator / denominator) * x
+    while abs(first - second) > eps:
+        i += 1
+        numerator *= numerator_step
+        denominator *= denominator_step
+
+        numerator_step += 2
+        denominator_step += 2
+        first = second
+
+        if i % 2 == 0:
+            second += (numerator / denominator) * x ** i
+        else:
+            second -= (numerator / denominator) * x ** i
+
+    return first
 
 
 # Kiểm tra với giá trị x = 0.5
-x = 0.5
-print(inv_sqrt_1_plus_x(x))  # Kết quả phải gần bằng 1/math.sqrt(1+x) = 1/math.sqrt(1.5) = 0.816496580927726
+print(f"Example Five: {exercise_five(0.5)}")
 
 
 # 6
-def sin(x, terms=10):
-    """Tính giá trị của hàm sin(x) bằng chuỗi Taylor với số lượng terms"""
-    result = 0
-    sign = 1
-    for n in range(terms):
-        numerator = x ** (2 * n + 1)
-        denominator = math.factorial(2 * n + 1)
-        term = sign * numerator / denominator
-        result += term
-        sign *= -1
-    return result
+def exercise_six(x):
+    i = 3
+
+    first = 1
+    second = 1 - (x ** i) / math.factorial(i)
+    y = 0
+
+    while abs(first - second) > eps:
+        i += 2
+        y += 1
+
+        first = second
+
+        if i % 2 == 0:
+            second -= x ** i / math.factorial(i)
+        else:
+            second += x ** i / math.factorial(i)
+
+    return first
 
 
-# Kiểm tra với giá trị x = pi/4
-x = math.pi / 4
-print(sin(x))  # Kết quả phải gần bằng math.sin(x) = math.sin(pi/4) = 0.7071067811865476
+print(f"Example Six: {exercise_six(1)}")
 
 
 # 7
-def cos(x, terms=10):
-    """Tính giá trị của hàm cos(x) bằng chuỗi Taylor với số lượng terms"""
-    result = 0
-    sign = 1
-    for n in range(terms):
-        numerator = x ** (2 * n)
-        denominator = math.factorial(2 * n)
-        term = sign * numerator / denominator
-        result += term
-        sign *= -1
-    return result
+def exercise_seven(x):
+    i = 2
+
+    first = 1
+    second = 1 - (x ** i) / math.factorial(i)
+    y = 0
+
+    while abs(first - second) > eps:
+        y += 1
+        i += 2
+
+        first = second
+
+        if i % 2 != 0:
+            second -= x ** i / math.factorial(i)
+        else:
+            second += x ** i / math.factorial(i)
+
+    return first
 
 
-# Kiểm tra với giá trị x = pi/4
-x = math.pi / 4
-print(cos(x))  # Kết quả phải gần bằng math.cos(x) = math.cos(pi/4) = 0.7071067811865476
+print(f"Example Seven: {exercise_seven(1)}")
 
 
-# 8
-def arcsin(x, terms=10):
-    """Tính giá trị của hàm arcsin(x) bằng chuỗi Taylor với số lượng terms"""
-    result = 0
-    for n in range(terms):
-        numerator = 1
-        for i in range(2 * n):
-            numerator *= i + 1
-            if i % 2 == 1:
-                numerator *= x
-        denominator = 2 ** (2 * n) * math.factorial(n) ** 2 * (2 * n + 1)
-        term = numerator / denominator
-        result += term
-    return result
+# # 8
+def exercise_eight(x):
+    i = 3
+    first = x
+    numerator = 1
+    denominator = 2
+    second = first + ((numerator / denominator) * (x ** i) / i)
+    while abs(first - second) > eps:
+        i += 2
+        numerator = numerator * (i - 2)
+        denominator = denominator * (i - 1)
+
+        first = second
+        second = first + ((numerator / denominator) * (x ** i) / i)
+    return first
 
 
-# Kiểm tra với giá trị x = 0.5
-x = 0.5
-print(arcsin(x))  # Kết quả phải gần bằng math.asin(x) = math.asin(0.5) = 0.5235987755982989
+# Kiểm tra kết quả với x = 0.5
+print(f"Example Eight: {exercise_eight(0.5)}")
 
 
 # 9
-def sin_over_x(x, n):
-    """
-    Tính giá trị sin(x)/x với n số lượng các số hạng của chuỗi Taylor
-    """
-    result = 1
-    sign = -1
-    factorial = 1
-    power = x
+def exercise_nine(x):
+    step = 2
+    i = 0
+    first = 1
+    second = (first - x ** step / math.factorial(step + 1))
 
-    for i in range(1, n + 1):
-        sign *= -1
-        power *= x ** 2
-        factorial *= (2 * i - 1) * (2 * i)
-        result += sign * power / factorial
-
-    return result
+    while abs(first - second) > eps:
+        step += 2
+        i += 1
+        first = second
+        if i % 2 != 0:
+            second = first + x ** step / math.factorial(step + 1)
+        else:
+            second = first - x ** step / math.factorial(step + 1)
+    return first
 
 
 # Kiểm tra kết quả với x = 1 và n = 5
-x = 1
-n = 5
-print(f"sin({x})/{x} = {sin_over_x(x, n)}")
-print(f"Giá trị chính xác: {math.sin(x) / x}")
+print(f"Example Nine: {exercise_nine(1)}")
 
 
 # 10
-def arctan(x, n):
-    result = 0
-    for i in range(n):
-        sign = (-1) ** i
-        term = x ** (2 * i + 1) / (2 * i + 1)
-        result += sign * term
-    return result
+def exercise_ten(x):
+    step = 3
+    i = 0
+    first = x
+    second = (first - x ** step / step)
+
+    while abs(first - second) > eps:
+        step += 2
+        i += 1
+        first = second
+        if i % 2 != 0:
+            second = first + x ** step / step
+        else:
+            second = first - x ** step / step
+
+    return first
 
 
-x = 0.5  # Giá trị của x
-n = 10  # Số lượng các số hạng cần tính
-
-# Tính giá trị của arctan(x) theo công thức
-result = arctan(x, n)
-
-# Tính giá trị chính xác của arctan(x) bằng hàm của thư viện math
-exact_result = math.atan(x)
-
-# In ra kết quả
-print(f"arctan({x}) = {result}")
-print(f"Giá trị chính xác của arctan({x}) = {exact_result}")
+# Kiểm tra kết quả với x = 0.5
+print(f"Example Ten: {exercise_ten(0.5)}")
 
 
 # 11
-def ln_1plusx(x, n):
-    result = 0
-    sign = 1
-    for i in range(1, n + 1):
-        term = (x ** i) / i
-        result += sign * term
-        sign *= -1
-    return result
+def exercise_eleven(x):
+    step = 3
+    first = x
+    second = first + x ** step / step
+
+    while abs(first - second) > eps:
+        step += 2
+        first = second
+        second = first + x ** step / step
+    return first
 
 
-x = 0.5  # Giá trị của x
-n = 10  # Số lượng các số hạng cần tính
-
-# Tính giá trị của ln(1+x) theo công thức
-result = math.log(1 + x)
-
-# Tính giá trị của ln(1+x) bằng cách tính tổng các số hạng
-approx_result = ln_1plusx(x, n)
-
-# In ra kết quả
-print(f"ln(1+{x}) = {result}")
-print(f"Giá trị xấp xỉ của ln(1+{x}) = {approx_result}")
+# Kiểm tra kết quả với x = 0.5
+print(f"Example Eleven: {exercise_eleven(0.5)}")
 
 
 # 12
-def ln(x, n):
-    result = 0
-    for i in range(n):
-        term = (x ** (2 * i + 1)) / (2 * i + 1)
-        result += term
-    return 2 * result
+def exercise_twelve(x):
+    step = 3
+    first = x
+    second = first + x ** step / step
+
+    while abs(first - second) > eps:
+        step += 2
+        first = second
+        second = first + x ** step / step
+    return first
 
 
-x = 0.5  # Giá trị của x
-n = 10  # Số lượng các số hạng cần tính
-
-# Tính giá trị của ln(1+x/1-x) theo công thức
-result = ln(x, n)
-
-# Tính giá trị chính xác của ln(1+x/1-x) bằng hàm của thư viện math
-exact_result = math.log((1 + x) / (1 - x))
-
-# In ra kết quả
-print(f"ln(1+{x}/1-{x}) = {result}")
-print(f"Giá trị chính xác của ln(1+{x}/1-{x}) = {exact_result}")
+# Kiểm tra kết quả với x = 0.5
+print(f"Example Twelve: {exercise_twelve(0.5)}")
 
 
 # 13
-def sinh(x, n):
-    result = 0
-    for i in range(n):
-        term = (x ** (2 * i + 1)) / math.factorial(2 * i + 1)
-        result += term
-    return result
+def exercise_thirteen(x):
+    step = 3
+    first = x
+    second = first + x ** step / math.factorial(step)
+
+    while abs(first - second) > eps:
+        step += 2
+        first = second
+        second = first + x ** step / math.factorial(step)
+    return first
 
 
-x = 1.5  # Giá trị của x
-n = 10  # Số lượng các số hạng cần tính
-
-# Tính giá trị của sinh x theo công thức
-result = (math.exp(x) - math.exp(-x)) / 2
-
-# Tính giá trị của sinh x bằng cách tính tổng các số hạng
-approx_result = sinh(x, n)
-
-# In ra kết quả
-print(f"sinh({x}) = {result}")
-print(f"Giá trị xấp xỉ của sinh({x}) = {approx_result}")
+# Kiểm tra kết quả với x = 0.5
+print(f"Example Thirteen {exercise_thirteen(0.5)}")
 
 
 # 14
-def cosh(x, n):
-    result = 0
-    for i in range(n):
-        term = (x ** (2 * i)) / math.factorial(2 * i)
-        result += term
-    return result
+def exercise_fourteen(x):
+    step = 2
+    first = x
+    second = first + x ** step / math.factorial(step)
+
+    while abs(first - second) > eps:
+        step += 2
+        first = second
+        second = first + x ** step / math.factorial(step)
+    return first
 
 
-x = 1.5  # Giá trị của x
-n = 10  # Số lượng các số hạng cần tính
+# Kiểm tra kết quả với x = 0.5
+print(f"Example Fourteen: {exercise_fourteen(0.5)}")
 
-# Tính giá trị của cosh x theo công thức
-result = (math.exp(x) + math.exp(-x)) / 2
-
-# Tính giá trị của cosh x bằng cách tính tổng các số hạng
-approx_result = cosh(x, n)
-
-# In ra kết quả
-print(f"cosh({x}) = {result}")
-print(f"Giá trị xấp xỉ của cosh({x}) = {approx_result}")
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Exercise-end')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
